b8093c822b4caed3e0c4e37a9b97d94b4605bc2d
isailara <isailara20002@gmail.com>
2022-02-22 17:53:56 -0600

    List

--- src/uaslp/objetos/list/Main.java
+++ src/uaslp/objetos/list/Main.java
@@ -1,5 +1,9 @@
 package uaslp.objetos.list;
 
+import uaslp.objetos.list.arraylist.ArrayList;
+import uaslp.objetos.list.arraylist.ArrayListIterator;
+
+
 import uaslp.objetos.list.linkedlist.LinkedList;
 import uaslp.objetos.list.linkedlist.LinkedListIterator;
 
@@ -7,7 +11,119 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        LinkedList team1 = new LinkedList();
+
+        ArrayList team1 = new ArrayList();
+        ArrayList team2 = new ArrayList();
+        ArrayList team3 = new ArrayList();
+
+        team1.addAtTail("Jesús");
+        team1.addAtTail("Salomón");
+        team1.addAtTail("Yael");
+
+        team2.addAtFront("Cristian");
+        team2.addAtFront("Daniel");
+        team2.addAtFront("Diego");
+
+        team3.addAtFront("Imelda");
+
+        ArrayListIterator iterator;
+
+        iterator = team1.getIterator();
+
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+
+        // Debió haber impreso
+        // Jesús
+        // Salomón
+        // Yael
+        System.out.println();
+        iterator = team2.getIterator();
+
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+
+        // Debió haber impreso
+        // Diego
+        // Daniel
+        // Cristian
+
+        System.out.println();
+        team1.remove(0);
+        team1.addAtFront("Rebeca");
+        System.out.println("Team 1 tiene: " + team1.getSize() + " integrantes"); // debe imprimir "Team 1 tiene 3 integrantes"
+
+        iterator = team1.getIterator();
+
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+
+        // Debió haber impreso
+        // Rebeca
+        // Salomón
+        // Yael
+        System.out.println();
+        team2.remove(2);
+        team2.addAtTail("Rita");
+        System.out.println("Team 2 tiene: " + team2.getSize() + " integrantes"); // debe imprimir "Team 2 tiene 3 integrantes"
+
+        iterator = team2.getIterator();
+
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+
+        // Debió haber impreso
+        // Diego
+        // Daniel
+        // Rita
+
+        System.out.println();
+        team3.remove(0);
+        team3.remove(0); // El elemento no existe pero el programa no debe cerrarse por algún error
+
+        team3.addAtTail("Tadeo");
+        team3.addAtFront("Isai");
+
+        System.out.println("Team 3 tiene: " + team3.getSize() + " integrantes"); // debe imprimir "Team 3 tiene 2 integrantes"
+
+        iterator = team3.getIterator();
+
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+
+        // Debió haber impreso
+        // Tadeo
+        // Isai
+
+        if(team1.getAt(1).equals("Salomón")){
+            team1.setAt(1, "Luis");
+        }
+
+        System.out.println();
+        System.out.println("Team 1 tiene: " + team1.getSize() + " integrantes"); // debe imprimir "Team 1 tiene 3 integrantes"
+
+        iterator = team1.getIterator();
+
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+
+        // Debió haber impreso
+        // Rebeca
+        // Luis
+        // Yael
+        /*LinkedList team1 = new LinkedList();
         LinkedList team2 = new LinkedList();
         LinkedList team3 = new LinkedList();
 
@@ -117,6 +233,6 @@ public class Main {
         // Debió haber impreso
         // Rebeca
         // Luis
-        // Yael
+        // Yael*/
     }
 }
